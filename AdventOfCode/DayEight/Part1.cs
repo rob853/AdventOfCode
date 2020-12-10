930c3332589ff40d2a80f0cfaed5b64784975035
Robert Hansen <robert.hansen@bglgroup.co.uk>
2020-12-10 10:43:12 +0000

    Day 8 part 1 done

--- AdventOfCode/DayEight/Part1.cs
+++ AdventOfCode/DayEight/Part1.cs
@@ -1,19 +1,46 @@
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace AdventOfCode.DayEight
 {
     public class Part1
     {
-        public int GetAnswer(){
-            var data = DataGetter.GetData();
+        private int _answer;
+        private List<InstructionModel> _data;
 
-            var answer = 0;
+        public int GetAnswer()
+        {
+            _data = DataGetter.GetData();
+            ProcessInstruction(_data[0]);
 
-            foreach (var instruction in data)
+            return _answer;
+        }
+
+        private void ProcessInstruction(InstructionModel instruction)
+        {
+            if (instruction.Completed) return;
+
+            InstructionModel nextInstruction;
+            switch (instruction.Instruction)
             {
-                
-                
+                case "acc":
+                    _answer += instruction.Direction == "+" ? instruction.Value : -instruction.Value;
+                    nextInstruction = _data.First(a => a.Index == instruction.Index + 1);
+                    break;
+                case "jmp":
+                    var nextIndex = instruction.Index + (instruction.Direction == "+" ? instruction.Value : -instruction.Value);
+                    nextInstruction = _data.First(a => a.Index == nextIndex);
+                    break;
+                case "nop":
+                    nextInstruction = _data.First(a => a.Index == instruction.Index + 1);
+                    break;
+                default:
+                    throw new InvalidDataException("Invalid data at index " + instruction.Index);
             }
 
-            return 0;
+            instruction.Completed = true;
+            ProcessInstruction(nextInstruction);
         }
     }
 }
